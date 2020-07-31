76277960f879e8351f387b4e70eb9942dcafdda6
Cristina Tarantino <cristina@tarantino.io>
2020-07-31 13:47:22 +0100

    Updates

---
+++ precache-manifest.20c51e511ee33e2caad1353476f13f8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913aeb4fc2a9ae6136cda56b737457da",
+    "url": "/CristyTarantino/react-quote-machine/index.html"
+  },
+  {
+    "revision": "9d0a070fc783883711a6",
+    "url": "/CristyTarantino/react-quote-machine/static/css/main.8c8c33ed.chunk.css"
+  },
+  {
+    "revision": "fdb5511d7431a6465aaf",
+    "url": "/CristyTarantino/react-quote-machine/static/js/2.d54fdd7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CristyTarantino/react-quote-machine/static/js/2.d54fdd7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d0a070fc783883711a6",
+    "url": "/CristyTarantino/react-quote-machine/static/js/main.33484db8.chunk.js"
+  },
+  {
+    "revision": "77f9cb14b684e51d45e0",
+    "url": "/CristyTarantino/react-quote-machine/static/js/runtime-main.d9b8506b.js"
+  }
+]);
